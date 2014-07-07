66a89057d54fcff4f5b4e76142a381a050782f3e
Erin Millard <ezzatron@gmail.com>
2014-07-07 15:57:26 +1000

    Update to latest Pathogen.

--- src/Symbol/SymbolReference.php
+++ src/Symbol/SymbolReference.php
@@ -191,6 +191,32 @@ class SymbolReference extends RelativePath implements SymbolReferenceInterface
     }
 
     /**
+     * Creates a new path instance of the most appropriate type.
+     *
+     * This method is called internally every time a new path instance is
+     * created as part of another method call. It can be overridden in child
+     * classes to change which classes are used when creating new path
+     * instances.
+     *
+     * @param mixed<string> $atoms                The path atoms.
+     * @param boolean       $isAbsolute           True if the new path should be absolute.
+     * @param boolean|null  $hasTrailingSeparator True if the new path should have a trailing separator.
+     *
+     * @return PathInterface The newly created path instance.
+     */
+    protected function createPath(
+        $atoms,
+        $isAbsolute,
+        $hasTrailingSeparator = null
+    ) {
+        if ($isAbsolute) {
+            return QualifiedSymbol::constructUnsafe($atoms, $hasTrailingSeparator);
+        }
+
+        return SymbolReference::constructUnsafe($atoms, $hasTrailingSeparator);
+    }
+
+    /**
      * Get the symbol factory.
      *
      * @return SymbolFactoryInterface The symbol factory.
