76f5ef034ff0edc18bb0d070f6c50ec4ad6dcac3
Erin Millard <ezzatron@gmail.com>
2015-08-27 20:35:51 +1000

    Implemented symbol reference generator.

---
+++ src/Resolution/SymbolReferenceGenerator.php
@@ -0,0 +1,129 @@
+<?php
+
+/*
+ * This file is part of the Cosmos package.
+ *
+ * Copyright © 2015 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Eloquent\Cosmos\Resolution;
+
+use Eloquent\Cosmos\Resolution\Context\ResolutionContextInterface;
+use Eloquent\Cosmos\Symbol\SymbolFactory;
+use Eloquent\Cosmos\Symbol\SymbolFactoryInterface;
+use Eloquent\Cosmos\Symbol\SymbolInterface;
+
+/**
+ * Generates references to qualified symbols.
+ */
+class SymbolReferenceGenerator implements SymbolReferenceGeneratorInterface
+{
+    /**
+     * Get a static instance of this generator.
+     *
+     * @return SymbolReferenceGeneratorInterface The static generator.
+     */
+    public static function instance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new self(SymbolFactory::instance());
+        }
+
+        return self::$instance;
+    }
+
+    /**
+     * Construct a new symbol resolver.
+     *
+     * @param SymbolFactoryInterface $symbolFactory The symbol factory to use.
+     */
+    public function __construct(SymbolFactoryInterface $symbolFactory)
+    {
+        $this->symbolFactory = $symbolFactory;
+    }
+
+    /**
+     * Find the shortest symbol that will resolve to the supplied qualified
+     * symbol from within the supplied resolution context.
+     *
+     * If the qualified symbol is not a child of the primary namespace, and
+     * there are no related use statements, this method will return a qualified
+     * symbol.
+     *
+     * @param ResolutionContextInterface $context The resolution context.
+     * @param SymbolInterface            $symbol  The symbol to resolve.
+     * @param string|null                $type    The symbol type.
+     *
+     * @return SymbolInterface The shortest symbol.
+     */
+    public function referenceTo(
+        ResolutionContextInterface $context,
+        SymbolInterface $symbol,
+        $type = null
+    ) {
+        $atoms = $symbol->atoms();
+        $namespace = $context->primaryNamespace();
+        $namespaceAtoms = $namespace->atoms();
+        $numNamespaceAtoms = \count($namespaceAtoms);
+
+        $isDescendant =
+            $namespaceAtoms === \array_slice($atoms, 0, $numNamespaceAtoms);
+
+        if ($isDescendant) {
+            // symbol is descendant of namespace
+            $match = \array_slice($atoms, $numNamespaceAtoms);
+
+            if (!$match) {
+                // symbol is exactly equal to namespace
+                return $symbol;
+            }
+
+            if ($context->symbolByAtom($match[0], $type)) {
+                // use statment clashes, use namespace keyword
+                \array_unshift($match, 'namespace');
+            }
+        } else {
+            $match = $atoms;
+        }
+
+        $matchSize = \count($match);
+
+        foreach ($context->useStatementsByType($type) as $useStatement) {
+            if ($matchSize < 2) {
+                break;
+            }
+
+            foreach ($useStatement->clauses() as $clause) {
+                $clauseAtoms = $clause->symbol()->atoms();
+                $numClauseAtoms = \count($clauseAtoms);
+                $isDescendant =
+                    $clauseAtoms === \array_slice($atoms, 0, $numClauseAtoms);
+
+                if ($isDescendant) {
+                    // symbol is descendant of use statement
+                    $thisMatch = \array_slice($atoms, $numClauseAtoms);
+                    \array_unshift($thisMatch, $clause->effectiveAlias());
+                    $thisMatchSize = \count($thisMatch);
+
+                    if ($thisMatchSize < $matchSize) {
+                        // this match is better
+                        $match = $thisMatch;
+                        $matchSize = $thisMatchSize;
+                    }
+                }
+            }
+        }
+
+        if ($matchSize >= count($atoms)) {
+            return $symbol;
+        }
+
+        return $this->symbolFactory->createFromAtoms($match, false);
+    }
+
+    private static $instance;
+    private $symbolFactory;
+}
---
+++ test/suite/Resolution/SymbolReferenceGeneratorTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the Cosmos package.
+ *
+ * Copyright © 2015 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Eloquent\Cosmos\Resolution;
+
+use Eloquent\Cosmos\Resolution\Context\ResolutionContext;
+use Eloquent\Cosmos\Symbol\Symbol;
+use Eloquent\Cosmos\Symbol\SymbolFactory;
+use Eloquent\Cosmos\UseStatement\UseStatement;
+use Eloquent\Liberator\Liberator;
+use PHPUnit_Framework_TestCase;
+
+class SymbolReferenceGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->symbolFactory = new SymbolFactory();
+        $this->subject = new SymbolReferenceGenerator($this->symbolFactory);
+
+        $this->primaryNamespace = Symbol::fromString('\SymbolA\SymbolB');
+        $this->useStatements = array(
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolC\SymbolD')),
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolE\SymbolF'), 'SymbolG'),
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolH\SymbolI')),
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolH\SymbolI\SymbolJ')),
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolA\SymbolB\SymbolAC')),
+
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolM\SymbolN'), null, 'function'),
+            UseStatement::fromSymbol(Symbol::fromString('\SymbolU\SymbolV'), null, 'const'),
+        );
+        $this->context = new ResolutionContext($this->primaryNamespace, $this->useStatements);
+    }
+
+    public function referenceToData()
+    {
+        //                                                 symbol                                      type        expected
+        return array(
+            'Primary namespace'                   => array('\SymbolA\SymbolB',                         null,       '\SymbolA\SymbolB'),
+            'Primary namespace +1'                => array('\SymbolA\SymbolB\SymbolC',                 null,       'SymbolC'),
+            'Primary namespace +2'                => array('\SymbolA\SymbolB\SymbolC\SymbolD',         null,       'SymbolC\SymbolD'),
+            'Primary namespace +3'                => array('\SymbolA\SymbolB\SymbolC\SymbolD\SymbolE', null,       'SymbolC\SymbolD\SymbolE'),
+            'Use statement'                       => array('\SymbolC\SymbolD',                         null,       'SymbolD'),
+            'Use statement +1'                    => array('\SymbolC\SymbolD\SymbolE',                 null,       'SymbolD\SymbolE'),
+            'Use statement +2'                    => array('\SymbolC\SymbolD\SymbolE\SymbolF',         null,       'SymbolD\SymbolE\SymbolF'),
+            'Alias'                               => array('\SymbolE\SymbolF',                         null,       'SymbolG'),
+            'Alias +1'                            => array('\SymbolE\SymbolF\SymbolH',                 null,       'SymbolG\SymbolH'),
+            'Alias +2'                            => array('\SymbolE\SymbolF\SymbolH\SymbolI',         null,       'SymbolG\SymbolH\SymbolI'),
+            'Shortest use statement'              => array('\SymbolH\SymbolI\SymbolJ',                 null,       'SymbolJ'),
+            'Use statement not too short'         => array('\SymbolH\SymbolI\SymbolG',                 null,       'SymbolI\SymbolG'),
+            'No relevant statements'              => array('\Foo\Bar\Baz',                             null,       '\Foo\Bar\Baz'),
+            'Use statement better than namespace' => array('\SymbolA\SymbolB\SymbolAC\SymbolAD',       null,       'SymbolAC\SymbolAD'),
+            'Avoid use statement clash'           => array('\SymbolA\SymbolB\SymbolD',                 null,       'namespace\SymbolD'),
+            'Avoid use statement clash + N'       => array('\SymbolA\SymbolB\SymbolD\SymbolE\SymbolF', null,       'namespace\SymbolD\SymbolE\SymbolF'),
+            'Avoid use alias clash'               => array('\SymbolA\SymbolB\SymbolG',                 null,       'namespace\SymbolG'),
+            'Avoid use alias clash + N'           => array('\SymbolA\SymbolB\SymbolG\SymbolE\SymbolF', null,       'namespace\SymbolG\SymbolE\SymbolF'),
+
+            'Use statement (function)'            => array('\SymbolM\SymbolN',                         'function', 'SymbolN'),
+            'Use statement (constant)'            => array('\SymbolU\SymbolV',                         'const',    'SymbolV'),
+        );
+    }
+
+    /**
+     * @dataProvider referenceToData
+     */
+    public function testReferenceTo($symbolString, $type, $expected)
+    {
+        $this->assertSame(
+            $expected,
+            strval($this->subject->referenceTo($this->context, Symbol::fromString($symbolString), $type))
+        );
+    }
+
+    public function testInstance()
+    {
+        $class = get_class($this->subject);
+        $liberatedClass = Liberator::liberateClass($class);
+        $liberatedClass->instance = null;
+        $actual = $class::instance();
+
+        $this->assertInstanceOf($class, $actual);
+        $this->assertSame($actual, $class::instance());
+    }
+}
